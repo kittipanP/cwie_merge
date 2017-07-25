c7ee1fa94cc7a58d6f84fb42901b1f5b5cff736b
KITTIPAN <kittipan.prasertsang@gmail.com>
2017-07-25 14:41:41 +0700

    first commit

---
+++ z.php
@@ -0,0 +1,89 @@
+        
+        <!-- third start -->
+        <div class="w3-col l12">
+            <div class="">
+
+                <!-- I/III -->
+                <div class="w3-third">
+                  <div class="">
+
+                    <div class="w3-row-padding w3-center w3-margin-top">
+                      <div class="w3-panel w3-gray w3-card-8 w3-center-align"><p> ..I..</p></div>
+                      
+
+
+                    </div> 
+                        
+                  </div>
+                </div>
+
+                <!-- II/III-->
+                <div class="w3-third">
+                    <div class="" style="">
+                    <div class="w3-row-padding w3-center w3-margin-top">
+                       
+                      <div class="w3-panel w3-gray w3-card-8 w3-center-align"><p> ..II..</p></div>
+
+
+
+                    </div> 
+                    </div>
+                </div>
+
+                <!--  III/III-->
+                <div class="w3-third">
+                    <div class=""  style="">
+                    <div class="w3-row-padding w3-center w3-margin-top">    
+
+                      <div class="w3-panel w3-gray w3-card-8 w3-center-align"><p>..III..</p></div>
+                        
+
+
+
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+
+        <!-- third end -->
+        </div>
+
+
+                    <div class="w3-col w3-container" style="width:45%">
+                        <div align="left">
+                        <label for="">Project Name : </label>
+                        </div>
+                        <input type="hidden" name="project_id" value="" size="32" placeholder="" /> 
+                        <input type="text" name="project_name" value="" size="32" placeholder="Smart CWIE Database Management System" />  
+                    </div>
+                    <div class="w3-col w3-container" style="width:55%">
+                        <div align="left">
+                        <label for="">Project Detail : </label>
+                        </div>
+                        <input type="text" name="project_detail" value="" size="32" placeholder="" />
+                    </div>
+
+                        <input type="hidden" name="thp_id" value="" size="32" placeholder="" />
+                        <input type="hidden" name="project_id" value="<?php echo $row_prjSet['project_id']+1?>" size="32" placeholder="" />
+                        <input type="hidden" name="trainee_id" value="<?php echo $row_tniSet['trainee_id']+1?>" size="32" placeholder="" />
+
+
+
+
+
+                        <div align="left">
+                        <label for="">Project Name : </label>
+                        </div>
+                        <input type="hidden" name="project_id" value="" size="32" placeholder="" /> 
+                        <input type="text" name="project_name" value="" size="32" placeholder="Smart CWIE Database Management System" />  
+                    
+                        <div align="left">
+                        <label for="">Project Detail : </label>
+                        </div>
+                        <textarea name="project_detail" value="" size="32" placeholder=""></textarea>
+                        
+            
+                        <input type="hidden" name="thp_id" value="" size="32" placeholder="" />
+                        <input type="hidden" name="project_id" value="<?php echo $row_prjSet['project_id']+1?>" size="32" placeholder="" />
+                        <input type="hidden" name="trainee_id" value="<?php echo $row_tniSet['trainee_id']+1?>" size="32" placeholder="" />
